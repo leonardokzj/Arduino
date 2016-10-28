77fb89c4007445f9c97987215a402f7429a04c16
leonardokzj <leonardokzj@hotmail.com>
2016-10-28 19:38:06 -0200

    Add files via upload

---
+++ Led/BibliotecaLed/BibliotecaLed.ino
@@ -0,0 +1,19 @@
+#include <Led.h>
+
+Led led = Led(11);
+Led led2 = Led(3);
+
+void setup() {
+  pinMode(led.getPorta, OUTPUT);
+  pinMode(led2.getPorta, OUTPUT);
+}
+
+void loop() {
+  led.acender();
+  Delay(100);
+  led.apagar();
+  Delay(100);
+  led.inverter();
+  DELAY(100);
+  led.piscar(10, 5000);
+}
